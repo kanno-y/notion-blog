e7c2de4adefc7ea3b90da5d6bc42e916df654171
kanno-y <kanno0521@gmail.com>
2023-04-12 08:43:01 +0900

    :pencil2: ISRでNotionで投稿した記事を全て取得する

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
-import Image from 'next/image'
+import { GetStaticProps } from 'next'
 import { Inter } from 'next/font/google'
+import { getAllPosts } from '@/lib/notionAPI'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export async function getStaticProps<GetStaticProps>(context: any) {
+  const allPosts: any = await getAllPosts()
+  return {
+    props: { allPosts },
+    revalidate: 60, // 60秒ごと更新する(ISR)
+  }
+}
+
+export default function Home(allPosts: any) {
+  console.log('allPosts', allPosts)
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
