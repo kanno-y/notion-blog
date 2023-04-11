9719ef49a0ec4b575e5a6f8be354200aca6efe15
kanno-y <kanno0521@gmail.com>
2023-04-12 08:03:44 +0900

    :pencil2: Add: NotionAPI用のライブラリをインストール

---
+++ lib/notionAPI.ts
@@ -0,0 +1,15 @@
+import { Client } from '@notionhq/client'
+
+// Initializing a client
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+})
+
+export const getAllPosts = async () => {
+  const posts = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID as string,
+    page_size: 100,
+  })
+  const allPosts = posts.results
+  return allPosts
+}
