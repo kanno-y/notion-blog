a5126aef29948d9869030ed96934cd4ab4782289
kanno-y <kanno0521@gmail.com>
2023-05-06 16:12:33 +0900

    コミット忘れ

---
+++ components/Pagination/Pagination.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import React from 'react'
+
+type Props = {
+  numberOfPage: number
+}
+export const Pagination = ({ numberOfPage }: Props) => {
+  let pages: number[] = []
+  for (let i = 1; i <= numberOfPage; i++) {
+    pages.push(i)
+  }
+  return (
+    <section className="mb-8 lg:w-1/2 mx-auto p-5">
+      <ul className="flex items-center justify-center gap-4">
+        {pages.map((page) => (
+          <li key={page} className="bg-sky-900 rounded-lg w-6 h-8 relative">
+            <Link
+              className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 text-gray-100"
+              href={`/posts/page/${page}`}
+            >
+              {page}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  )
+}
